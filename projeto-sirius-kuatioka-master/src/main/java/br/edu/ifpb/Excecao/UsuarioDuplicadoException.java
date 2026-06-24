9ed9c323e8301df6d1a7c8b60f7cc766d5099ff4
PAULOLIMA6 <paulo.lima.6@academico.ifpb.edu.br>
2026-06-24 16:33:03 -0300

    Add UsuarioDuplicadoException.java

---
+++ projeto-sirius-kuatioka-master/src/main/java/br/edu/ifpb/Excecao/UsuarioDuplicadoException.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.excecao;
+
+public class UsuarioDuplicadoException extends Exception {
+    public UsuarioDuplicadoException(String mensagem) {
+        super(mensagem);
+    }
+}
+
