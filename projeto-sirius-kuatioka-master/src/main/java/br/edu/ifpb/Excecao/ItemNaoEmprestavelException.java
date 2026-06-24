2de7cc305a459708b8ce86b20b2b4952361898af
PAULOLIMA6 <paulo.lima.6@academico.ifpb.edu.br>
2026-06-24 16:26:04 -0300

    Add ItemNaoEmprestavelException.java

---
+++ projeto-sirius-kuatioka-master/src/main/java/br/edu/ifpb/Excecao/ItemNaoEmprestavelException.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.excecao;
+
+public class ItemNaoEmprestavelException extends Exception {
+    public ItemNaoEmprestavelException(String mensagem) {
+        super(mensagem);
+    }
+}
+
