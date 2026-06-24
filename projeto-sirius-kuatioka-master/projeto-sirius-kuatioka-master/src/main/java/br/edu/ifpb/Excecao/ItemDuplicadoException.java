0b786742abb0e8347eb6fdc9f24e6b53fc2ac54d
PAULOLIMA6 <paulo.lima.6@academico.ifpb.edu.br>
2026-06-24 15:58:27 -0300

    Removendo pastas duplicadas e organizando o projeto

--- projeto-sirius-kuatioka-master/projeto-sirius-kuatioka-master/src/main/java/br/edu/ifpb/Excecao/ItemDuplicadoException.java
+++
@@ -1,7 +0,0 @@
-package br.edu.ifpb.excecao;
-
-public class ItemDuplicadoException extends Exception {
-    public ItemDuplicadoException(String mensagem) {
-        super(mensagem);
-    }
-}
--- projeto-sirius-kuatioka-master/projeto-sirius-kuatioka-master/src/main/java/br/edu/ifpb/modelo/Emprestavel.java
+++
@@ -1,5 +0,0 @@
-package br.edu.ifpb.modelo;
-
-public interface Emprestavel {
-    // interface vazia só pra identificar o q pode passar por empréstimo
-}
