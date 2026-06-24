5f28232b022fd8c466f9ba77c17fe6b98dadf014
PAULOLIMA6 <paulo.lima.6@academico.ifpb.edu.br>
2026-06-24 16:14:58 -0300

    Add  a exceção ItemNaoDisponivelException

---
+++ projeto-sirius-kuatioka-master/src/main/java/br/edu/ifpb/Excecao/ItemNaoDisponivelException.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.excecao;
+
+public class ItemNaoDisponivelException extends Exception {
+    public ItemNaoDisponivelException(String mensagem) {
+        super(mensagem);
+    }
+}
+
