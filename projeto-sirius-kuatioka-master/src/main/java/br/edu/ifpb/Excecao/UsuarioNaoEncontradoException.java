5b42163e4fb255c65fd89fdc2bd545aad30a9916
PAULOLIMA6 <paulo.lima.6@academico.ifpb.edu.br>
2026-06-24 16:36:55 -0300

    Add UsuarioNaoEncontradoException.java

---
+++ projeto-sirius-kuatioka-master/src/main/java/br/edu/ifpb/Excecao/UsuarioNaoEncontradoException.java
@@ -0,0 +1,8 @@
+package br.edu.ifpb.excecao;
+
+public class UsuarioNaoEncontradoException extends Exception {
+    public UsuarioNaoEncontradoException(String mensagem) {
+        super(mensagem);
+    }
+}
+
